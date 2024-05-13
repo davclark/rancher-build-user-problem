26fdd1d748af1cadfaf3ea498b18504f806116b7
Dav Clark <dav.clark@crowdstrike.com>
2024-05-13 15:49:48 -0400

    Simplify, move to python3.10

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # Why does taht fail? Because we ran as root (with test's $HOME) so we have libs readable only by root
-docker run -it --rm -u test problem_amd bash -lc "httpx --help"
+docker run -it --rm -u test problem_amd python3 -c 'import httpx'
